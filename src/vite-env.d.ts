4ac279d724683244b277e91508dd8bfe9e5ab1e4
chasinalts <chasinalts@gmail.com>
2025-04-21 15:30:01 -0500

    Fix TypeScript type definitions for React and React DOM

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,39 @@
 /// <reference types="vite/client" />
+
+declare module 'react' {
+  import * as React from 'react';
+  export = React;
+}
+
+declare module 'react-dom' {
+  import * as ReactDOM from 'react-dom';
+  export = ReactDOM;
+}
+
+declare module 'react/jsx-runtime' {
+  export const jsx: any;
+  export const jsxs: any;
+  export const Fragment: any;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
